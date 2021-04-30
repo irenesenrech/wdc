7ca10ef1c607d8fcd079c1a26d163920b5fe5fdd
irenesenrech <83312263+irenesenrech@users.noreply.github.com>
2021-04-30 11:43:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
+console.log("¡Funciona!");
+
 (function () {
   var myConnector = tableau.makeConnector();
+
   myConnector.getSchema = function (schemaCallback) {
     const cols = [
       {
-        id: "lastupdatedate",
+        id: "ID",
         dataType: tableau.dataTypeEnum.string,
       },
       {
-        id: "type",
+        id: "Tipologia",
         dataType: tableau.dataTypeEnum.string,
       },
       {
-        id: "id",
+        id: "FechaHora",
         dataType: tableau.dataTypeEnum.string,
       },
       {
-        id: "datetime",
-        dataType: tableau.dataTypeEnum.string,
+        id: "Valor",
+        dataType: tableau.dataTypeEnum.int,
       },
       {
-        id: "percentage",
+        id: "Porcentaje",
         dataType: tableau.dataTypeEnum.float,
       },
       {
-        id: "value",
-        dataType: tableau.dataTypeEnum.float,
+        id: "Ultima_actualizacion",
+        dataType: tableau.dataTypeEnum.string,
       },
     ];
 
     let apiTableSchema = {
-      id: "APIDATOS",
-      alias: "Data from API",
+      id: "REData",
+      alias: "Datos de la REData API para estructura-generacion",
       columns: cols,
     };
 
@@ -41,23 +44,24 @@
     let tableData = [];
     var i = 0;
     var j = 0;
-    //var today = Date.now().toDateString().slice(0, 16);
+
     $.getJSON(
-      "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=2021-04-26T00:00&end_date=2021-04-27T00:00&time_trunc=hour",
+      "https://apidatos.ree.es/es/datos/generacion/estructura-generacion?start_date=2021-04-27T00:00&end_date=2021-04-27T23:59&time_trunc=day",
       function (resp) {
         var apiData = resp.included;
+        // Iterate over the JSON object
         for (i = 0, len = apiData.length; i < len; i++) {
-            for (j = 0; j < apiData[i].attributes.values.length; j++) {
-                var dic = apiData[i].attributes.values[j];
-                tableData.push({
-                    datetime: dic.datetime,
-                    percentage: dic.percentage,
-                    value: Number(dic.value),
-                    lastupdatedate: resp.data.attributes["last-update"],
-                    type: apiData[i].type,
-                    id: apiData[i].id,
-                });
-            }
+          for (j = 0; j < apiData[i].attributes.values.length; j++) {
+            var nestedData = apiData[i].attributes.values[j];
+            tableData.push({
+              FechaHora: nestedData.datetime,
+              Porcentaje: nestedData.percentage,
+              Valor: Number(nestedData.value),
+              Ultima_actualizacion: resp.data.attributes["last-update"],
+              Tipologia: apiData[i].type,
+              ID: apiData[i].id,
+            });
+          }
         }
         table.appendRows(tableData);
         doneCallback();
@@ -71,6 +75,6 @@
 document.querySelector("#getData").addEventListener("click", getData);
 
 function getData() {
-  tableau.connectionName = "API Datos";
+  tableau.connectionName = "REData";
   tableau.submit();
 }
