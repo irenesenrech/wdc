08e0921e96f2b74e79ab9937d6a1e83bc9daab2c
irenesenrech <83312263+irenesenrech@users.noreply.github.com>
2021-04-30 11:27:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -43,7 +43,7 @@
     var j = 0;
     var today = Date.now().toDateString().slice(0, 16);
     $.getJSON(
-      "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+"2021-04-27T00:00"+"&end_date="+"2021-04-27T00:00"+"&time_trunc=hour",
+      "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=2021-04-27T00:00&end_date=2021-04-27T00:00&time_trunc=hour",
       function (resp) {
         var apiData = resp.included;
         for (i = 0, len = apiData.length; i < len; i++) {
